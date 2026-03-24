d60c2dda7ebc98a24855a82f9534c2a92605d9d8
rapha713 <33769016+rapha713@users.noreply.github.com>
2026-03-24 12:33:29 -0300

    Update build script to include progress messages

--- script.sh
+++ script.sh
@@ -1,4 +1,7 @@
-echo "Rodando build do projeto..."
-echo "Data atual:"
-date
-echo "Finalizado!"
+echo "Build iniciado"
+for i in 1 2 3 4 5
+do
+  echo "Etapa $i executando..."
+  sleep 1
+done
+echo "Build finalizado"
